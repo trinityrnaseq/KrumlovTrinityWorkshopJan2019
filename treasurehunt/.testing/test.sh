9480779a5f129104ddebe4b8965b3e69b6561faa
Brian Haas <brianjohnhaas@gmail.com>
2019-01-10 16:14:44 +0100

    updated

--- treasurehunt/.testing/test.sh
+++ treasurehunt/.testing/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./encode_msg.pl "LINE01 something cool
+./encode_msg.pl --encode "LINE01 something cool
 LINE02 whatever
 LINE03 ultra cool" > ladeda.fa
 
-./encode_msg.pl ./ladeda.fa
+./encode_msg.pl --decode ./ladeda.fa
 
 
 
